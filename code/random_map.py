17f8fb2fc51f59b82a64be7ed06c99d41a605c63
Danilka06 <main@0x1f44.ru>
2023-12-30 00:11:23 +0300

    refactor: map_fill func

--- code/random_map.py
+++ code/random_map.py
@@ -1,6 +1,6 @@
 from numpy import floor
 from perlin_noise import PerlinNoise
-import matplotlib.pyplot as plt
+import time
 
 
 class RandomMap:
@@ -13,21 +13,22 @@ class RandomMap:
         self.length = 15
         self.wight = 7
 
-        self.landscale = [[0 for _ in range(self.length)] for _ in range(self.wight)]
-
-        for x in range(self.wight):
-            for z in range(self.length):
-
-                y = floor(self.noise([x / self.period, z / self.period]) * self.amp)
-                # print(f"x-{x}, z-{z}, y-{y}")
-                self.landscale[int(x)][int(z)] = abs(int(y))
+        self.map = [[0 for _ in range(self.length)] for _ in range(self.wight)]
+        self.map_fill()
 
         self.pprint()
+
+    def map_fill(self):
+        for x in range(self.wight):
+            for z in range(self.length):
+                y = floor(self.noise([x / self.period, z / self.period]) * self.amp)
+                # print(f"x-{x}, z-{z}, y-{y}")
+                self.map[int(x)][int(z)] = abs(int(y))
 
     def pprint(self):
         for y in range(self.wight):
             for x in range(self.length):
-                item = self.landscale[y][x]
+                item = self.map[y][x]
                 if item == 1:
                     print(f"\033[31m{item}\033[0m", end="")
                 else:
